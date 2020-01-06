97e56710cfa88ac0810d337206d7925adf111594
Cosmo473 <cosmo473@gmail.com>
2020-01-06 18:34:44 -0300

    Fim Modelo Conceitual

---
+++ src/main/java/com/cosmo/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.cosmo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cosmo.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
---
+++ src/main/java/com/cosmo/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.cosmo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cosmo.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
